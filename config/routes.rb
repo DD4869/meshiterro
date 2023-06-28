0b9c9bd74646057ced1d55139323f1059c59362c
DD4869 <110597693+DD4869@users.noreply.github.com>
2023-06-28 03:37:39 +0000

    Second Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
   root to: 'homes#top'
   get '/homes/about' => 'homes#about', as: 'about'
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  # 投稿画像にコメントのため、post_commentsはpost_imagesに結びつく
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:show, :edit, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
